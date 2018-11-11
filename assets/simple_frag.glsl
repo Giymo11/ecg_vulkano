2adfe4d49734ec7dea25f8fa93951ae6ba38b863
Giymo11 <gizmo.head@yahoo.de>
2018-11-11 23:09:15 +0100

    Implements Task1 and Task2

---
+++ assets/simple_frag.glsl
@@ -0,0 +1,20 @@
+#version 450
+
+layout(location = 0) in vec3 v_normal;
+
+layout(push_constant) uniform PushData {
+    vec4 color;
+} pushdata;
+
+layout(location = 0) out vec4 f_color;
+
+const vec3 LIGHT = vec3(0.0, 0.0, 1.0);
+
+void main() {
+    float brightness = dot(normalize(v_normal), normalize(LIGHT));
+    vec3 dark_color = vec3(0.6, 0.0, 0.0);
+    vec3 regular_color = vec3(1.0, 0.0, 0.0);
+
+    f_color = pushdata.color;
+}
+
